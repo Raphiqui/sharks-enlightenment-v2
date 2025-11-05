34950e76926625bb196fc7da20d9345c61d48f13
norsse <raphael.pastre@gmail.com>
2025-11-05 19:56:20 +0100

    Add back and front init folders

---
+++ back/back/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-s^ow_phw6&m($pile)w5xwewy$(!@_k8+(ow#!83&aqvcl51!r"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
