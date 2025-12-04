0fb4977efc21ac96d02abccaca2f5dccf13b91b3
norsse <raphael.pastre@gmail.com>
2025-12-04 20:53:50 +0100

    Boilerplate models

--- back/home/models.py
+++ back/home/models.py
@@ -1,7 +1,35 @@
 from django.db import models
 
 from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
+from django.utils.translation import gettext_lazy as _
+from wagtail_localize.fields import TranslatableField
 
 
 class HomePage(Page):
-    pass
+    body = RichTextField(_("Body"), blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("body"),
+    ]
+
+    translatable_fields = [
+        TranslatableField("body"),
+    ]
+
+
+class AboutPage(Page):
+
+    parent_page_types = ["home.HomePage"]
+
+    body = RichTextField(_("Body"), blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("body")
+    ]
+
+    translatable_fields = [
+        TranslatableField("body")
+    ]
+
