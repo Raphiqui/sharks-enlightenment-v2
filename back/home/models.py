2f95aef3c5f8488af000a03ba387d659b4022fd3
norsse <raphael.pastre@gmail.com>
2025-12-06 15:47:05 +0100

    Create thumbnail model

--- back/home/models.py
+++ back/home/models.py
@@ -5,7 +5,14 @@ from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
 from django.utils.translation import gettext_lazy as _
 from wagtail_localize.fields import TranslatableField
-
+from wagtail.images import get_image_model_string
+from wagtail.blocks import (
+    CharBlock,
+    StructBlock,
+)
+from wagtail.fields import StreamField
+from wagtail import blocks
+from wagtail.images.blocks import ImageChooserBlock
 
 class HomePage(Page):
     body = RichTextField(_("Body"), blank=True)
@@ -18,8 +25,18 @@ class HomePage(Page):
         TranslatableField("body"),
     ]
 
+class SharkThumbnail(StructBlock):
 
-class AboutPage(Page):
+    name = CharBlock(help_text=_("Name of the shark"), required=True, label=_(""))
+
+    image = ImageChooserBlock(
+        required=True,
+        help_text=_("Image of the shark"),
+        label=_("Image"),
+    )
+
+    class Meta:
+        template = "sharks/thumbnail.html"
 
     parent_page_types = ["home.HomePage"]
 
