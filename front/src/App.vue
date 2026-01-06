967c59ffbfacb668ac20760dad8260b6b393682a
norsse <raphael.pastre@gmail.com>
2026-01-06 20:12:35 +0100

    Remove boilerplate

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
     <h1>Hello from Vue + Wagtail!</h1>
-    <p>This is working with django-vite</p>
-    <div class="container py-4 px-3 mx-auto">
-      <h1>Hello, Bootstrap and Vite!</h1>
-      <button class="btn btn-primary">Primary button</button>
-    </div>
   </div>
 </template>
 
