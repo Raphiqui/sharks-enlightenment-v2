33816b573a1e4373f0acfc8d04e5695fbf4b47c1
norsse <raphael.pastre@gmail.com>
2026-01-11 21:28:18 +0100

    Dynamic image serve view

--- back/back/urls.py
+++ back/back/urls.py
@@ -1,11 +1,12 @@
 from django.conf import settings
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.contrib import admin
 from django.conf.urls.i18n import i18n_patterns
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
+from wagtail.images.views.serve import ServeView
 
 from search import views as search_views
 
@@ -14,6 +15,7 @@ urlpatterns = [
     path("django-admin/", admin.site.urls),
     path("admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
+    re_path(r'^images/([^/]*)/(\d*)/([^/]*)/[^/]*$', ServeView.as_view(), name='wagtailimages_serve'),
 ]
 
 
