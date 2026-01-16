a5f040e01e6641ca0bbc253f3876fe179c1b9554
norsse <raphael.pastre@gmail.com>
2026-01-16 23:41:19 +0100

    Api response for Quiz

--- back/back/api.py
+++ back/back/api.py
@@ -1,14 +1,55 @@
 from ninja import NinjaAPI
+from typing import Any, List
 from home.models import QuizPage
 from ninja import Schema
+from pydantic import model_validator
 
 api = NinjaAPI()
 
-class QuizShema(Schema):
-    id: int
-    title: str
+class ResponseSchema(Schema):
+    response: str
+    is_correct: bool
 
-@api.get("/quiz", response=QuizShema)
+
+class QuestionSchema(Schema):
+    question: str
+    responses: List[ResponseSchema]
+
+
+class QuizSchema(Schema):
+    title: str
+    questions: List[QuestionSchema]
+
+    @model_validator(mode='before')
+    @classmethod
+    def extract_quiz_data(cls, data):
+        if hasattr(data, "quiz"):
+            questions = []
+            for block in data.quiz:
+
+                for question_struct in block.value:
+
+                    responses = []
+                    
+                    for resp_block in question_struct["responses"]:
+                        responses.append({
+                            "response": resp_block.value["response"],
+                            "is_correct": resp_block.value["is_correct"]
+                        })
+                    
+                    questions.append({
+                        "question": question_struct["question"],
+                        "responses": responses
+                    })
+            
+            return {
+                "title": data.title,
+                "questions": questions
+            }
+        return data
+
+
+@api.get("/quiz", response=QuizSchema)
 def get_quiz(request):
     quiz = QuizPage.objects.first()
     return quiz
