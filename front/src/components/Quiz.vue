6b802d2a91d921a2cfa3b2762ee561e8aca57cba
norsse <raphael.pastre@gmail.com>
2026-01-17 08:19:53 +0100

    Get quiz data using fetch

--- front/src/components/Quiz.vue
+++ front/src/components/Quiz.vue
@@ -1,10 +1,24 @@
 <template>
   <div>
-    <h1>Quiz from the frontend part</h1>
+    <div v-if="error">Oops! Error encountered: {{ error.message }}</div>
+    <div v-else-if="data">
+      Data loaded:
+      <pre>{{ data }}</pre>
+    </div>
+    <div v-else>Loading...</div>
   </div>
 </template>
 
 <script setup>
+  import { ref } from 'vue'
+
+  const data = ref(null)
+  const error = ref(null)
+
+  fetch("/api/quiz")
+    .then((res) => res.json())
+    .then((json) => (data.value = json))
+    .catch((err) => (error.value = err))
 </script>
 
 <style scoped>
