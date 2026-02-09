8539ea8be21c679eeb5762bc8c6145dd8666bfb1
norsse <raphael.pastre@gmail.com>
2026-02-09 23:20:24 +0100

    Add color theme

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,11 +1,15 @@
 export default {
-    content: [
-        "./index.html",
-        "./src/**/*.{vue,js,ts,jsx,tsx}",
-        "./backend/**/*.html",
-    ],
-    theme: {
-        extend: {},
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./backend/**/*.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "ocean-glow": "#00d9ff",
+      },
     },
-    plugins: [],
-}
+  },
+  plugins: [],
+};
