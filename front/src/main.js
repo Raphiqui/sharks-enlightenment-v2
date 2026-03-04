107eca41130428a97c98dd89180ff62aaba04b2a
norsse <raphael.pastre@gmail.com>
2026-03-04 21:55:03 +0100

    Most likely the last intent

--- front/src/main.js
+++ front/src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import './tailwind.css'
 import './style.scss'
 import Quiz from './components/Quiz.vue'
+@source "../back/templates/**/*.html";
 
 createApp(Quiz).mount('#quiz')
