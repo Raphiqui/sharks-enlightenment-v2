736a08bfa04f226c9e7f877859f5a7316f05604f
norsse <raphael.pastre@gmail.com>
2026-03-06 20:38:55 +0100

    Rm @source directive as not js and fix source directive path

--- front/src/main.js
+++ front/src/main.js
@@ -1,7 +1,6 @@
-import { createApp } from 'vue'
-import './tailwind.css'
-import './style.scss'
-import Quiz from './components/Quiz.vue'
-@source "../back/templates/**/*.html";
+import { createApp } from "vue";
+import "./tailwind.css";
+import "./style.scss";
+import Quiz from "./components/Quiz.vue";
 
-createApp(Quiz).mount('#quiz')
+createApp(Quiz).mount("#quiz");
