44215462bb0d9c4acbc1f7b270fde8bb8d6113e7
norsse <raphael.pastre@gmail.com>
2026-03-08 16:58:55 +0100

    Avoid error when element not found

--- front/src/main.js
+++ front/src/main.js
@@ -2,5 +2,14 @@ import { createApp } from "vue";
 import "./tailwind.css";
 import "./style.scss";
 import Quiz from "./components/Quiz.vue";
+import ScrollIndicator from "./components/ScrollIndicator.vue";
 
-createApp(Quiz).mount("#quiz");
+const quizEl = document.querySelector("#quiz");
+if (quizEl) {
+  createApp(Quiz).mount(quizEl);
+}
+
+const scrollIndicatorEl = document.querySelector("#scroll-indicator");
+if (scrollIndicatorEl) {
+  createApp(ScrollIndicator).mount(scrollIndicatorEl);
+}
